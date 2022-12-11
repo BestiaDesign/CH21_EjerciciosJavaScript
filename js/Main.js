bebd48d8be85e9fb70b1993294266b01e0bff30d
Johanna <bestiadesign1@gmail.com>
2022-12-11 02:53:02 -0600

    Se añadió el ejercicio 2

--- js/Main.js
+++ js/Main.js
@@ -1,14 +1,14 @@
     // EJERCICIO 1:
         // Solución 1
-    /* let numeros = document.getElementById("Mayores");
+    /* let numeros = document.getElementById("mayores");
     let numMayor = Math.max(3, 5, 7, 1, 9, 15);
     numeros.innerHTML += '<li class="list-group-item">' + numMayor + '</li>';
 */
 
     // Solución 2
-let numeros = document.getElementById("Mayores");
+let numeros = document.getElementById("mayores");
     let arrayNumeros  = [3, 2, 7, 1, 9, 15];
-    numMayor = 0;
+    let numMayor = 0;
 
 for(let i=0; i<arrayNumeros.length;i++){
     if(arrayNumeros [i] > numMayor){
@@ -31,5 +31,19 @@ document.write(numMayor); */
 
 
     // EJERCICIO 2:
-        // Solución 1
+        // Solución
+let arrayMenores = [-1, 3, 4, 2, 7, 25];
+let menores = document.getElementById("menores");
+let numeroMenor = 0;
+let i = 0;
 
+while (i<arrayNumeros.length) {
+    if(arrayNumeros [i] < numeroMenor){
+        numeroMenor = arrayNumeros [i];
+    }
+    i++;
+}
+menores.innerHTML += 'El número menor de -1, 3, 4, 2, 7, 25 es: ' + numeroMenor ; 
+
+    // EJERCICIO 3:
+        // Solución 1
