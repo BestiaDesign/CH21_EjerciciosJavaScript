7badfdef6b55a09cbb523b5960bbe97d1dbc29fb
Johanna <bestiadesign1@gmail.com>
2022-12-10 23:37:33 -0600

    Se actualizó el HTML del Index

---
+++ js/Main.js
@@ -0,0 +1,7 @@
+let num
+let numeros = ["3", "5", "7", "1", "8"]
+console.log(numeros.length);
+
+for (let index = 0; index < libros.numeros; index++) {
+    console.log( numeros[index].toUpperCase());
+}
