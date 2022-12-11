df4f532b1fb2094b295c41c5013602bf14172a26
Johanna <bestiadesign1@gmail.com>
2022-12-11 04:00:53 -0600

    Se agregó ejercicio 3

--- js/Main.js
+++ js/Main.js
@@ -47,3 +47,44 @@ menores.innerHTML += 'El número menor de -1, 3, 4, 2, 7, 25 es: ' + numeroMenor
 
     // EJERCICIO 3:
         // Solución 1
+    /* let array1 = [1, 22, 5, 17, 10, 5, 40, 5];
+    let array2 = [7, 41, 5, 7, 10, 13, 2];
+    let array3 = [1, 22, 5, 14, 24, 31, 27, 15, 105];
+    let coincidencia = document.getElementById("coincidencia");
+
+    let arrayX = array1.sort();
+     
+    for (let i = 0; i < arrayX.length; i++) {
+      if (arrayX[i + 1] === arrayX[i]) {
+        coincidencia.innerHTML += 'El '+ arrayX[i] + ' se encuentra más de una vez' ; 
+        break;
+      }
+    } */
+
+    // Solución 2
+    let array1 = [1, 22, 5, 17, 10, 5, 40, 5];
+    let array2 = [7, 41, 5, 7, 10, 13, 2];
+    let array3 = [1, 22, 5, 14, 24, 31, 27, 15, 105];
+    let coincidencia = document.getElementById("coincidencia");
+
+    function repetidos(array){
+        let bandera = 0; // La bandera para saber si hay las coincidencias en array
+        let arrayX = array.sort(); // sort sirve para ordenar arrays
+        for (let i = 0; i < arrayX.length; i++) {
+            if (arrayX[i + 1] === arrayX[i]) {
+              coincidencia.innerHTML += 'El '+ arrayX[i] + ' se encuentra más de una vez en ' + array + '<br>' ; 
+              bandera = 1;
+              break; // Para parar el ciclo de la función
+            }
+          }
+        if (bandera == 0) {
+            coincidencia.innerHTML += 'No hay números repetidos en ' + array + '<br>' ;         
+        }
+    } 
+    
+     repetidos(array1);
+     repetidos(array2);
+     repetidos(array3);
+     
+    // EJERCICIO 4:
+        // Solución 1
